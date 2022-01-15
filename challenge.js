6c38c1de93873088d9beaf4436ea37f3b09e038e
Fadhil Mulyono <fadhilmulyono@gmail.com>
2022-01-15 10:58:10 +0700

    Push parking app

---
+++ challenge.js
@@ -0,0 +1,53 @@
+// CHALLENGE-1
+// const benar = 3
+// const salah = -1
+// const kosong = 0
+
+// function cekJawaban (jawaban) {
+//     let kunciJawaban = ["A", "B", "C", "A", "B", "C"];
+//     let points = 0
+//     for (let i = 0; i < kunciJawaban.length; i++) {
+//         if (kunciJawaban[i] == jawaban[i]) {
+//             points += 3
+//         } else if (jawaban[i] == "") {
+//             points += 0
+//         } else {
+//             points -= 1
+            
+//         }
+//     } console.log(points)
+// }
+
+// let jawaban = ["A", "A", "", "B", "B", ""];
+// cekJawaban(jawaban)
+
+// CHALLENGE-2
+// let numbers = [1, -1, 3, 5, 10]
+
+// function addNumbers(numbers){
+//     let result = 0
+//     for(let i = 0; numbers.length; i++)
+//     if (numbers[i] >= 0) {
+//         result += numbers[i]
+//     } else {
+//         result += 0
+//     } console.log(result)
+// }
+
+// addNumbers(numbers)
+
+// Chalenge-3
+// Ex. Text : "Abcdefghijkl"
+//     Output   : "1 2 3 4 ..."
+
+// Task-1
+// Carilah kata terpendek
+// Input: Purwadhika Technology School
+// Output: School merupakan kata terpendek, karena hanya memiliki 3 huruf
+
+// Task-2
+// Multiplication Table
+// Input: 3
+// Output: 1 2 3
+// 2 4 6
+// 3 6 9
